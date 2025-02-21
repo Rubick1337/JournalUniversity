c1e59e340addf8bac6f4929c26594b67dcc64a02
Mikalay05 <kicluk@bk.ru>
2025-02-21 10:55:11 +0300

    feat: firt run server

---
+++ server/routes/index.js
@@ -0,0 +1,22 @@
+const { message } = require('../error/ApiError');
+
+const router = require('express').Router();
+
+
+router.get('/',(req,res) => {
+    res.status(200).json({message: 'homepage'})
+})
+
+function initializeRoute(route, routeName) {
+    const newRouter = require(`./${route}`);
+    router.use(`/${routeName}`, newRouter);
+}
+
+
+// initializeRoute('role', 'role');
+// initializeRoute('user', 'user');
+// initializeRoute('projectStatus', 'projectStatus');
+// initializeRoute('information', 'information');
+// initializeRoute('project', 'project');
+
+module.exports = router;
