f414d011924d92ea5512ce848610e72a503a6fc4
Rubick1337 <poss1337@gmail.com>
2025-04-23 23:44:14 +0300

    feat: add routers

---
+++ Client/journal/src/components/Routers/Routers.jsx
@@ -0,0 +1,169 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './RoutersStyle.css';
+
+import facultyIcon from '../../images/free-icon-faculty-3663869.png';
+import departmentIcon from '../../images/free-icon-lectern-1321023.png';
+import subjectIcon from '../../images/free-icon-book-3330314.png';
+import positionIcon from '../../images/free-icon-briefcase-1383186.png';
+import teacherIcon from '../../images/free-icon-teacher-10618211.png';
+import specializationIcon from '../../images/free-icon-compass-998887.png';
+import educationFormIcon from '../../images/free-icon-folders-2821739.png';
+import curriculumIcon from '../../images/free-icon-calendar-7955483.png';
+import programIcon from '../../images/free-icon-document-8931643.png';
+import groupIcon from '../../images/free-icon-couple-8890995.png';
+import subgroupIcon from '../../images/free-icon-single-person-5231019.png';
+import studentIcon from '../../images/free-icon-graduated-4537051.png';
+import lessonIcon from '../../images/free-icon-busy-4593078.png';
+import absenceIcon from '../../images/free-icon-clear-1632708.png';
+import gradeIcon from '../../images/free-icon-evaluation-6020452.png';
+
+const EduCards = () => {
+    const navigate = useNavigate();
+
+    const categories = [
+        {
+            title: 'Основные структуры',
+            cards: [
+                {
+                    id: 1,
+                    title: 'Факультеты',
+                    icon: facultyIcon,
+                    link: 'faculties'
+                },
+                {
+                    id: 2,
+                    title: 'Кафедры',
+                    icon: departmentIcon,
+                    link: 'departament'
+                },
+                {
+                    id: 3,
+                    title: 'Предметы',
+                    icon: subjectIcon,
+                    link: 'discipline'
+                }
+            ]
+        },
+        {
+            title: 'Персонал',
+            cards: [
+                {
+                    id: 4,
+                    title: 'Должности преподавателей',
+                    icon: positionIcon,
+                    link: 'positions'
+                },
+                {
+                    id: 5,
+                    title: 'Преподаватели',
+                    icon: teacherIcon,
+                    link: 'teachers'
+                }
+            ]
+        },
+        {
+            title: 'Учебные программы',
+            cards: [
+                {
+                    id: 6,
+                    title: 'Направления подготовки',
+                    icon: specializationIcon,
+                    link: 'specilization'
+                },
+                {
+                    id: 7,
+                    title: 'Формы подготовки',
+                    icon: educationFormIcon,
+                    link: 'TODO'
+                },
+                {
+                    id: 8,
+                    title: 'Учебные планы',
+                    icon: curriculumIcon,
+                    link: 'specilization'
+                },
+                {
+                    id: 9,
+                    title: 'Рабочие программы',
+                    icon: programIcon,
+                    link: 'curriculum'
+                }
+            ]
+        },
+        {
+            title: 'Студенты',
+            cards: [
+                {
+                    id: 10,
+                    title: 'Группы',
+                    icon: groupIcon,
+                    link: 'specilization'
+                },
+                {
+                    id: 11,
+                    title: 'Подгруппы',
+                    icon: subgroupIcon,
+                    link: 'specilization'
+                },
+                {
+                    id: 12,
+                    title: 'Студенты',
+                    icon: studentIcon,
+                    link: 'specilization'
+                }
+            ]
+        },
+        {
+            title: 'Учебный процесс',
+            cards: [
+                {
+                    id: 13,
+                    title: 'Занятия',
+                    icon: lessonIcon,
+                    link: 'specilization'
+                },
+                {
+                    id: 14,
+                    title: 'Пропуски',
+                    icon: absenceIcon,
+                    link: 'specilization'
+                },
+                {
+                    id: 15,
+                    title: 'Оценки',
+                    icon: gradeIcon,
+                    link: 'specilization'
+                }
+            ]
+        }
+    ];
+
+    return (
+        <div className="edu-container">
+            <h1 className="edu-header">Управление образовательным процессом</h1>
+
+            {categories.map((category, index) => (
+                <div key={index} className="category-section">
+                    <h2 className="category-title">{category.title}</h2>
+                    <div className="cards-grid">
+                        {category.cards.map(card => (
+                            <div
+                                key={card.id}
+                                className="edu-card"
+                                onClick={() => navigate(card.link)}
+                            >
+                                <div className="card-icon">
+                                    <img src={card.icon} alt={card.title} />
+                                </div>
+                                <h3>{card.title}</h3>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default EduCards;
