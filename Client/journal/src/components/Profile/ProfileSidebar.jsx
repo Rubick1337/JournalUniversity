e29a5bef092869dad553f11802f05b6eba241d68
Rubick1337 <poss1337@gmail.com>
2025-05-15 19:26:37 +0300

    feat: add abstance for profile

--- Client/journal/src/components/Profile/ProfileSidebar.jsx
+++ Client/journal/src/components/Profile/ProfileSidebar.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Typography, Box } from '@mui/material';
+import { useSelector, useDispatch } from 'react-redux';
+import { getForStudent } from '../../store/slices/absenteeismSlice';
 import avatar from '../../images/user_default.png';
 import './ProfileStyle.css';
 
 const ProfileSidebar = ({ profile }) => {
+    const dispatch = useDispatch();
+    const { user } = useSelector(state => state.user);
+    const absenteeismData = useSelector(state => state.absenteeism.studentAbsenteeism.data);
+    console.log(absenteeismData);
+    // Загружаем данные о прогулах при монтировании компонента
+    useEffect(() => {
+
+            dispatch(getForStudent(user.student_id));
+    }, [dispatch, user?.student_id]);
+
+    const excusedAbsences = absenteeismData?.data?.totals?.excusedHours || 0;
+    const unexcusedAbsences = absenteeismData?.data?.totals?.unexcusedHours || 0;
+
     return (
         <Box className="profile-sidebar">
             <img src={avatar} width={130} height={130} className="avatar-img" alt="Аватар"/>
@@ -14,11 +29,11 @@ const ProfileSidebar = ({ profile }) => {
                 <h4 className="section-title-profile">Прогулы</h4>
                 <div className="absences-container">
                     <div className="absence-item">
-                        <div className="absence-number excused">{profile.excusedAbsences || 0}</div>
+                        <div className="absence-number excused">{excusedAbsences}</div>
                         <div className="absence-label">По уважительной</div>
                     </div>
                     <div className="absence-item">
-                        <div className="absence-number unexcused">{profile.unexcusedAbsences || 0}</div>
+                        <div className="absence-number unexcused">{unexcusedAbsences}</div>
                         <div className="absence-label">Без уважительной</div>
                     </div>
                 </div>
